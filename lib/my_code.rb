1a73d29dc38c7968a1aed2b44a70285091eba0f9
allison cortez <allizetorc@gmail.com>
2019-10-02 21:46:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,6 +64,15 @@ def reduce_to_all_true(array)
   return true
 end
 
+def reduce_to_any_true(array)
+  counter = 0
+  while counter < array.size do
+    return false if #write condition here
+    counter += 1
+  end
+  return true
+end
+
 
 
  
