97b2db1b53bd81f9b72c01a694a5e6dfeaff3b2f
allison cortez <allizetorc@gmail.com>
2019-10-02 21:19:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,6 +45,16 @@ def map_to_square(array)
   final_array
 end
 
+def reduce_to_total(array,starting_point=0)
+  total = starting_point
+  counter = 0
+  while counter < array.size do
+    total += array[counter]
+    counter += 1
+  end
+  total
+end
+
  
 
 
